83a38e82258be4be8d9518b55995586c0c663f03
nlaris <nick@larisnet.com>
2023-12-11 12:15:51 -0500

    Cleaning up Day 10

--- 2023/src/Day10.java
+++ 2023/src/Day10.java
@@ -31,63 +31,12 @@ public class Day10 {
                 row++;
                 line = br.readLine();
             }
-            map.set(currentCoords[1], map.get(currentCoords[1]).replace('S', getStartingChar()));
-            findPath();
+            final char startingChar = getStartingChar();
+            map.set(currentCoords[1], map.get(currentCoords[1]).replace('S', startingChar));
+            findPath(startingChar);
             System.out.println("Part 1: " + furthestSpot);
             System.out.println("Part 2: " + getEnclosedLocations());
         }
-    }
-
-    private static void findPath() {
-        String prevCoords = "";
-        char currentChar = getStartingChar();
-        int steps = 0;
-        while (!path.contains(translateCoord(currentCoords))) {
-            path.add(0, translateCoord(currentCoords));
-            currentCoords = getNextCoords(currentChar, prevCoords);
-            prevCoords = path.get(0);
-            currentChar = map.get(currentCoords[1]).charAt(currentCoords[0]);
-            steps++;
-        }
-        furthestSpot = steps / 2;
-    }
-
-    private static int getEnclosedLocations() {
-        int sum = 0;
-        for (int row = 1; row < map.size() - 1; row++) {
-            for (int col = 1; col < map.get(0).length() - 1; col++) {
-                if (!path.contains(translateCoord(new int[]{col, row}))) {
-                    int totalEdges = 0;
-                    Character neededCorner = null;
-                    for (int r = 0; r < row; r++) {
-                        if (path.contains(translateCoord(new int[]{col, r}))) {
-                            char c = map.get(r).charAt(col);
-                            if (c == '-') {
-                                totalEdges++;
-                            } else if (List.of('F', 'L', 'J', '7').contains(c)) {
-                                if (neededCorner != null) {
-                                    totalEdges += c == neededCorner ? 1 : 2;
-                                    neededCorner = null;
-                                } else {
-                                    neededCorner = c == '7' ? 'L' : 'J';
-                                }
-                            }
-                        }
-                    }
-                    sum += (totalEdges % 2);
-                }
-            }
-        }
-        return sum;
-    }
-
-    private static int[] getNextCoords(char currentChar, String prevCoords) {
-        int[][] conns = connectionCoords.get(currentChar);
-        int[] nextCoords = new int[] {currentCoords[0] + conns[0][0], currentCoords[1] + conns[0][1]};
-        if (translateCoord(nextCoords).equals(prevCoords)) {
-            nextCoords = new int[] {currentCoords[0] + conns[1][0], currentCoords[1] + conns[1][1]};
-        }
-        return nextCoords;
     }
 
     private static char getStartingChar() {
@@ -109,7 +58,60 @@ public class Day10 {
         return '|';
     }
 
+    private static void findPath(char currentChar) {
+        String prevCoords = "";
+        int steps = 0;
+        while (!path.contains(translateCoord(currentCoords))) {
+            path.add(0, translateCoord(currentCoords));
+            currentCoords = getNextCoords(currentChar, prevCoords);
+            prevCoords = path.get(0);
+            currentChar = map.get(currentCoords[1]).charAt(currentCoords[0]);
+            steps++;
+        }
+        furthestSpot = steps / 2;
+    }
+
+    private static int[] getNextCoords(char currentChar, String prevCoords) {
+        int[][] conns = connectionCoords.get(currentChar);
+        int[] nextCoords = new int[] {currentCoords[0] + conns[0][0], currentCoords[1] + conns[0][1]};
+        if (translateCoord(nextCoords).equals(prevCoords)) {
+            nextCoords = new int[] {currentCoords[0] + conns[1][0], currentCoords[1] + conns[1][1]};
+        }
+        return nextCoords;
+    }
+
     private static String translateCoord(int[] coord) {
         return coord[0] + " " + coord[1];
+    }
+
+    private static int getEnclosedLocations() {
+        int sum = 0;
+        for (int row = 1; row < map.size() - 1; row++) {
+            for (int col = 1; col < map.get(0).length() - 1; col++) {
+                if (!path.contains(translateCoord(new int[]{col, row}))) sum += countPathEdges(row, col);
+            }
+        }
+        return sum;
+    }
+
+    private static int countPathEdges(int row, int col) {
+        int totalEdges = 0;
+        Character neededCorner = null;
+        for (int r = 0; r < row; r++) {
+            if (path.contains(translateCoord(new int[]{col, r}))) {
+                char c = map.get(r).charAt(col);
+                if (c == '-') {
+                    totalEdges++;
+                } else if (List.of('F', 'L', 'J', '7').contains(c)) {
+                    if (neededCorner == null) {
+                        neededCorner = c == '7' ? 'L' : 'J';
+                    } else {
+                        totalEdges += c == neededCorner ? 1 : 2;
+                        neededCorner = null;
+                    }
+                }
+            }
+        }
+        return totalEdges % 2;
     }
 }
