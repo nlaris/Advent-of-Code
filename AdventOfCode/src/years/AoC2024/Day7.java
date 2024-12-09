969030b38c90558850fd132016d82e6b2e517d9f
Nick L <nlaris93@gmail.com>
2024-12-08 20:01:59 -0500

    2024 Day 7

---
+++ AdventOfCode/src/years/AoC2024/Day7.java
@@ -0,0 +1,42 @@
+package years.AoC2024;
+
+import common.Day;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Day7 implements Day {
+    public void run(BufferedReader reader) throws IOException {
+        String line;
+        long part1Sum = 0, part2Sum = 0;
+        while ((line = reader.readLine()) != null) {
+            long target = Long.parseLong(line.split(":")[0]);
+            long[] values = Arrays.stream(line.split(": ")[1].split(" "))
+                    .mapToLong(Long::parseLong)
+                    .toArray();
+            part1Sum += getScore(target, values, false);
+            part2Sum += getScore(target, values, true);
+        }
+        System.out.println("Part 1: " + part1Sum);
+        System.out.println("Part 2: " + part2Sum);
+    }
+
+    private long getScore(long target, long[] numbers, boolean part2) {
+        ArrayList<Long> results = new ArrayList<>();
+        results.add(numbers[0]);
+        for (int i = 1; i < numbers.length; i++) {
+            ArrayList<Long> newResults = new ArrayList<>();
+            for (Long num : results) {
+                newResults.add(num * numbers[i]);
+                newResults.add(num + numbers[i]);
+                if (part2) {
+                    newResults.add(Long.parseLong(num + "" + numbers[i]));
+                }
+            }
+            results = newResults;
+        }
+        return results.stream().anyMatch(x -> x.equals(target)) ? target : 0;
+    }
+}
