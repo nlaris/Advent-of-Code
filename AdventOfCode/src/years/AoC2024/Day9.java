8f7304e77913eec1511c6e366b045ab5358103a2
Nick L <nlaris93@gmail.com>
2024-12-09 14:35:02 -0500

    2024 Day 9

---
+++ AdventOfCode/src/years/AoC2024/Day9.java
@@ -0,0 +1,88 @@
+package years.AoC2024;
+
+import common.Day;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day9 implements Day {
+    @Override
+    public void run(BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        System.out.println("Part 1: " + part1(line));
+        System.out.println("Part 2: " + part2(line));
+    }
+
+    private long part1(String line) {
+        long part1Sum = 0;
+        int lastId = line.length() / 2, index = 0, endMarker = 1;
+        int endCount = Character.getNumericValue(line.charAt(line.length() - endMarker));
+        for (int i = 0; i <= line.length() - endMarker; i++) {
+            int num = Character.getNumericValue(line.charAt(i));
+            int j = 0, id = i / 2;
+            if (i % 2 == 0) {
+                while (j < num && (lastId > id || j < endCount)) {
+                    part1Sum += (long) index * id;
+                    j++;
+                    index++;
+                }
+            } else {
+                while (j < num && lastId >= id) {
+                    part1Sum += (long) index * lastId;
+                    if (--endCount == 0) {
+                        lastId--;
+                        endMarker += 2;
+                        endCount = Character.getNumericValue(line.charAt(line.length() - endMarker));
+                    }
+                    j++;
+                    index++;
+                }
+            }
+        }
+        return part1Sum;
+    }
+
+    private long part2(String line) {
+        long sum = 0;
+        int index = 0;
+        ArrayList<Integer> countedFiles = new ArrayList<>();
+        for (int i = 0; i < line.length() && countedFiles.size() <= line.length() / 2; i++) {
+            int num = Character.getNumericValue(line.charAt(i));
+            int j = 0, id = i / 2;
+            if (i % 2 == 0) {
+                if (!countedFiles.contains(id)) {
+                    while (j < num) {
+                        sum += (long) index * (id);
+                        j++;
+                        index++;
+                    }
+                    countedFiles.add(id);
+                } else {
+                    index += num;
+                }
+            } else {
+                int endMarker = 1;
+                while (j < num && endMarker <= line.length()) {
+                    while (endMarker <= line.length() && (countedFiles.contains((line.length() - endMarker) / 2) || (num - j) < Character.getNumericValue(line.charAt(line.length() - endMarker)))) {
+                        endMarker += 2;
+                    }
+                    if (endMarker <= line.length()) {
+                        countedFiles.add((line.length() - endMarker) / 2);
+                        int endCount = Character.getNumericValue(line.charAt(line.length() - endMarker)) + j;
+                        while (j < endCount) {
+                            sum += (long) index * ((line.length() - endMarker) / 2);
+                            j++;
+                            index++;
+                        }
+                    }
+                }
+                index += (num - j);
+            }
+        }
+        return sum;
+    }
+}
