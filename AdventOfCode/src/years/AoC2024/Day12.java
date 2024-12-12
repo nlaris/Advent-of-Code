a9a8f28d83d04ca28c664fbf382fea249499770b
Nick L <nlaris93@gmail.com>
2024-12-12 10:44:41 -0500

    unused imports

--- AdventOfCode/src/years/AoC2024/Day12.java
+++ AdventOfCode/src/years/AoC2024/Day12.java
@@ -5,11 +5,7 @@ import common.Day;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.OptionalInt;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Day12 implements Day {
 
