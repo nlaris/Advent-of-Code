41b138f2345d6eaf044d97e08147d0cbc09d4bd7
Nick L <nlaris93@gmail.com>
2025-12-03 08:50:43 -0500

    simplify

--- AdventOfCode/src/years/AoC2025/Day3.java
+++ AdventOfCode/src/years/AoC2025/Day3.java
@@ -6,28 +6,26 @@ import java.io.IOException;
 import common.Day;
 
 public class Day3 implements Day {
-
   @Override
   public void run(BufferedReader reader) throws IOException {
     String line;
     long part1Sum = 0, part2Sum = 0;
     while ((line = reader.readLine()) != null) {
-      part1Sum += getJoltage(line, 0, 2, "");
-      part2Sum += getJoltage(line, 0, 12, "");
+      part1Sum += getJoltage(line, 2, "");
+      part2Sum += getJoltage(line, 12, "");
     }
     System.out.println("Part 1: " + part1Sum);
     System.out.println("Part 2: " + part2Sum);
   }
 
-  private static long getJoltage(String bank, int start, int endOffset, String joltage) {
+  private static long getJoltage(String bank, int endOffset, String joltage) {
     if (endOffset == 0) {
       return Long.parseLong(joltage);
     }
     int index = -1, value = 10;
     while (index == -1) {
-      index = bank.substring(start, bank.length() - endOffset + 1).indexOf(String.valueOf(--value));
+      index = bank.substring(0, bank.length() - endOffset + 1).indexOf(String.valueOf(--value));
     }
-    joltage += value;
-    return getJoltage(bank, start + index + 1, endOffset - 1, joltage);
+    return getJoltage(bank.substring(index + 1), endOffset - 1, joltage + value);
   }
 }
